72e205435b4673f064e4c31f11ce617bc86f4552
Heather Piverotto <heather@Huberts-MacBook-Pro.local>
2019-01-18 01:24:58 +0100

    Complete defining action creators

--- src/actions/search.js
+++ src/actions/search.js
@@ -4,21 +4,32 @@ import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
-var handleVideoSearch = (q) => {
-  //TODO:  Write an asynchronous action to handle a video search!
-  console.log(q)
-  var cb = ()=>{console.log('this works!')};
-  var options = {
-    key: YOUTUBE_API_KEY,
-    query: q
-  }
-  searchYouTube(options, cb);
+// var handleVideoSearch = (q) => ({
+//   //TODO:  Write an asynchronous action to handle a video search!
+//   // console.log(q)
+//   // var cb = ()=>{console.log('this works!')};
+//   // var options = {
+//   //   key: YOUTUBE_API_KEY,
+//   //   query: q
+//   // }
+//   // searchYouTube(options, cb);
 
-  // return 
-  // {
+//   // return 
+//   // {
     
-  // }
+//   // }
+//   type: 'HANDLE_VIDEO_SEARCH',
+//   payload: q
 
-};
+// });
+
+var handleVideoSearch = (q) => {
+  return (dispatch) => {
+    return searchYouTube({key: YOUTUBE_API_KEY, query: q}, function(data) {
+      dispatch(changeVideoList(data));
+      dispatch(changeVideo(data[0]));
+    })
+  }
+}
 
 export default handleVideoSearch;
--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -2,6 +2,9 @@ import { connect } from 'react-redux';
 import VideoList from './../components/VideoList.js';
 import changeVideo from './../actions/currentVideo.js';
 
+//need to map state to props
+//need to map dispatch to props
+//then connect the mapstate, mapdispatch and videolist 
 var VideoListContainer = () => {};
 
 //TODO: define a VideoListContainer component which will hook up your action
