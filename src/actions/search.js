b2363a074d8d7563cdaf9341db70ccd18402d460
Mike Lopez <mikeglopez.91@gmail.com>
2019-01-19 12:57:22 -0600

    Complete Bare Minimum Requirements

--- src/actions/search.js
+++ src/actions/search.js
@@ -3,26 +3,6 @@ import changeVideoList from './videoList.js';
 import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
 
-
-// var handleVideoSearch = (q) => ({
-//   //TODO:  Write an asynchronous action to handle a video search!
-//   // console.log(q)
-//   // var cb = ()=>{console.log('this works!')};
-//   // var options = {
-//   //   key: YOUTUBE_API_KEY,
-//   //   query: q
-//   // }
-//   // searchYouTube(options, cb);
-
-//   // return 
-//   // {
-    
-//   // }
-//   type: 'HANDLE_VIDEO_SEARCH',
-//   payload: q
-
-// });
-
 var handleVideoSearch = (q) => {
   return (dispatch) => {
     return searchYouTube({key: YOUTUBE_API_KEY, query: q}, function(data) {
